63337c6772154789dbdfee33eaf7ce2ec29279d4
JennysaGyu <kimgyuvin31@gmail.com>
2023-12-08 09:39:24 +0700

    commit 1

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include 'header.php';?>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="400" src="https://s3.cosmopolitan.co.id/images/CSMZSuJXi_9_Fakta_Unik_Mingyu_SEVENTEEN,_Si_Rapper_Seksi_yang_Berbakat!_2.jpg" alt="image profil">
+            <h1 class ="display-4">Kim Mingyu</h1>
+            <p class="lead">Member Seventeen</p>
+        </div>
+      </div>
+
+       <div class="container text-center" id="about">
+            <div class="row">
+                <div class="col">
+                    <h1>About</h1>
+                 </div>
+            </div>
+       </div>
+
+       <div class="row">
+            <div class="col text-justify">
+            Kim Mingyu atau yang dikenal dengan Mingyu Seventeen baru saja merayakan pertambahan usianya yang ke 26 tahun pada hari ini, 6 April 2023. Berikut profil dan biodata Mingyu Seventeen. Menariknya, anggota Seventeen ini terpilih menjadi anggota yang paling tampan. Dirinya bahkan menduduki peringkat ke-49 dalam kategori The Most Handsome Faces pada 2017, pada tahun berikutnya, Mingyu juga masuk ke dalam The 100 Most Handsome Faces versi TC Candler.
+            </div>
+            <div class="col text-justify">
+            Mingyu Seventeen lahir dengan nama asli Kim Mingyu di provinsi Gyeonggi-do, Korea Selatan. Nama Min berarti batu permata berharga sedangkan nama Gyu mengandung arti bintang. Sejak kecil Mingyu dikenal sebagai anak multitalenta, dirinya terlibat dalam berbagai aktivitas di sekolahnya, salah satunya adalah bermain sepakbola. Bahkan, ketika SD dan SMP, Mingyu berhasil meraih juara 2 turnamen sepak bola nasional.
+            </div>
+       </div>
+
+      <div class="card-deck mt-5 ml-5 mr-5">
+        <div class="card">
+          <img class="card-img-top" width="300" height="300" src="https://pbs.twimg.com/media/EkDcLYXU8AADtMg.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Mingyu Era Left and Right</h5>
+            <p class="card-text">"Left & Right" sendiri mengusung konsep musik groovy pop dance yang pas untuk didengarkan di musim panas.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" width="300" height="300" src="https://pbs.twimg.com/media/FCSFp6nVgAI6a3k?format=jpg&name=large" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Mingyu Era Rock With You</h5>
+            <p class="card-text">Mengusung genre pop-rock, "Rock With You" dijelaskan tentang keinginan untuk terus bersama dengan orang yang dicintai.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" width="300" height="300" src="https://i.pinimg.com/736x/44/f2/2f/44f22f2366a717c584f91e1f7a422f68.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Mingyu Era Adore U</h5>
+            <p class="card-text">" Adore U "adalah lagu pop funky tentang seorang remaja laki-laki yang mencoba menavigasi melalui cinta monyet."</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+      <?php include 'form.php';?>
+      <?php include 'footer.php';?>
+      
+      
